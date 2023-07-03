8ed928a596c34650ab22ca59f42637932513e177
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-03 17:32:10 -0300

    Refactor: extrair função cpf#isEveryCharTheSame

--- backend/ride/src/cpf.ts
+++ backend/ride/src/cpf.ts
@@ -1,4 +1,8 @@
 // @ts-nocheck
+function isEveryCharTheSame(str: string): boolean {
+    return str.split("").every(c => c === str[0]);
+}
+
 export function validate (str) {
     if (!str) return false
     if (str.length < 11 || str.length > 14) return false
@@ -8,45 +12,45 @@ export function validate (str) {
         .replace('-', '')
         .replace(" ", "");
 
-    if (!str.split("").every(c => c === str[0])) {
-        try {
-            let d1, d2;
-            let dg1, dg2, rest;
-            let digito;
-            let nDigResult;
-            d1 = d2 = 0;
-            dg1 = dg2 = rest = 0;
+    if (isEveryCharTheSame(str)) return false;
 
-            for (let nCount = 1; nCount < str.length - 1; nCount++) {
-                // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                // 	return false;
-                // } else {
+    try {
+        let d1, d2;
+        let dg1, dg2, rest;
+        let digito;
+        let nDigResult;
+        d1 = d2 = 0;
+        dg1 = dg2 = rest = 0;
 
-                digito = parseInt(str.substring(nCount - 1, nCount));
-                d1 = d1 + (11 - nCount) * digito;
+        for (let nCount = 1; nCount < str.length - 1; nCount++) {
+            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
+            // 	return false;
+            // } else {
 
-                d2 = d2 + (12 - nCount) * digito;
-                // }
-            }
-            ;
+            digito = parseInt(str.substring(nCount - 1, nCount));
+            d1 = d1 + (11 - nCount) * digito;
 
-            rest = (d1 % 11);
-
-            dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
-            d2 += 2 * dg1;
-            rest = (d2 % 11);
-            if (rest < 2)
-                dg2 = 0;
-            else
-                dg2 = 11 - rest;
-
-            let nDigVerific = str.substring(str.length - 2, str.length);
-            nDigResult = "" + dg1 + "" + dg2;
-            return nDigVerific == nDigResult;
-        } catch (e) {
-            console.error("Erro !" + e);
-
-            return false;
+            d2 = d2 + (12 - nCount) * digito;
+            // }
         }
-    } else return false;
+        ;
+
+        rest = (d1 % 11);
+
+        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;
+        d2 += 2 * dg1;
+        rest = (d2 % 11);
+        if (rest < 2)
+            dg2 = 0;
+        else
+            dg2 = 11 - rest;
+
+        let nDigVerific = str.substring(str.length - 2, str.length);
+        nDigResult = "" + dg1 + "" + dg2;
+        return nDigVerific == nDigResult;
+    } catch (e) {
+        console.error("Erro !" + e);
+
+        return false;
+    }
 }
