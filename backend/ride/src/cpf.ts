254e2fc3f97f9aec3fe6abf478be139d1348f7b5
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-03 22:07:51 -0300

    refactor: utilizar array de numeros ao inves de string em calculteFirstDigit e calculateSecondDigit

--- backend/ride/src/cpf.ts
+++ backend/ride/src/cpf.ts
@@ -18,25 +18,23 @@ function isLengthValid(cpf: string) {
     return cpf.length === 11;
 }
 
-function calculateFirstDigit(cpf: string) {
+function calculateFirstDigit(digits: number[]) {
     let d1 = 0;
 
-    for (let nCount = 0; nCount < cpf.length - 2; nCount++) {
-        const digito = parseInt(cpf.substring(nCount, nCount + 1));
+    digits.forEach((digito, nCount) => {
         d1 = d1 + (10 - nCount) * digito;
-    }
+    })
 
     const rest = (d1 % 11);
     return (rest < 2) ? 0 : 11 - rest;
 }
 
-function calculateSecondDigit(cpf: string, d1: number) {
+function calculateSecondDigit(digits: number[], d1: number) {
     let d2 = 0;
 
-    for (let nCount = 0; nCount < cpf.length - 2; nCount++) {
-        const digito = parseInt(cpf.substring(nCount, nCount + 1));
+    digits.forEach((digito, nCount) => {
         d2 = d2 + (11 - nCount) * digito;
-    }
+    })
 
     const rest2 = ((d2 + 2 * d1) % 11);
     return rest2 < 2 ? 0 : 11 - rest2;
@@ -44,8 +42,9 @@ function calculateSecondDigit(cpf: string, d1: number) {
 
 function isValidCpf(cpf: string): boolean {
     if (!isLengthValid(cpf) || isEveryCharTheSame(cpf)) return false
-    const d1 = calculateFirstDigit(cpf);
-    const d2 = calculateSecondDigit(cpf, d1);
+    const digits = cpf.split("").slice(0, -2).map(it => parseInt(it))
+    const d1 = calculateFirstDigit(digits);
+    const d2 = calculateSecondDigit(digits, d1);
     return compareDigitsWithCpf(d1, d2, cpf);
 }
 
