ba754bdae68d4b3a8be524dfb41e74f694612a30
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-04 01:08:16 -0300

    refactor: cpf to use classes instead

--- backend/ride/src/cpf.ts
+++ backend/ride/src/cpf.ts
@@ -1,18 +1,4 @@
-function isEveryCharTheSame(str: string): boolean {
-    return str.split("").every(c => c === str[0]);
-}
-
-function filterOnlyNumbers(str: string): string {
-    return str.replace(/\D+/g, '')
-}
-
-function compareDigitsWithCpf(firstDigit: number, secondDigit: number, cpf: string) {
-    return cpf.slice(-2) == '' + firstDigit + secondDigit;
-}
-
-function isLengthValid(cpf: string) {
-    return cpf.length === 11;
-}
+import {LazyGetter} from 'lazy-get-decorator';
 
 function reduceByFactorOf(factor: number) {
     return (prev: number, next: number, index: number) => prev + (factor - index) * next
@@ -24,18 +10,64 @@ function calculateDigit(digits: number[], factor: number, previousDigit: number 
     return rest < 2 ? 0 : 11 - rest;
 }
 
-function cpfToDigits(cpf: string): number[] {
-    return cpf.split("").slice(0, -2).map(it => parseInt(it));
-}
-
-function isValidCpf(cpf: string): boolean {
-    if (!isLengthValid(cpf) || isEveryCharTheSame(cpf)) return false
-    const digits = cpfToDigits(cpf);
-    const firstDigit = calculateDigit(digits, 10);
-    const secondDigit = calculateDigit(digits, 11, firstDigit);
-    return compareDigitsWithCpf(firstDigit, secondDigit, cpf);
-}
-
 export function validate (str: string) {
-    return isValidCpf(filterOnlyNumbers(str))
+    return new Cpf(str).isValid
+}
+
+export class Cpf {
+    private readonly raw: string;
+
+    constructor(rawCpf: string) {
+        this.raw = rawCpf
+    }
+
+    @LazyGetter()
+    private get digits(): number[] {
+        return this.raw.replace(/\D+/g, '').split("").map(it => parseInt(it));
+    }
+
+    @LazyGetter()
+    private get verifiableDigits(): number[] {
+        return this.digits.slice(0, -2)
+    }
+
+    @LazyGetter()
+    private get givenVerifierDigits(): number[] {
+        return this.digits.slice(-2);
+    }
+
+    @LazyGetter()
+    private get firstDigit(): number {
+        return calculateDigit(this.verifiableDigits, 10);
+    }
+
+    @LazyGetter()
+    private get secondDigit(): number {
+        return calculateDigit(this.verifiableDigits, 11, this.firstDigit);
+    }
+
+    @LazyGetter()
+    private get calculatedVerifierDigits(): number[] {
+        return [this.firstDigit, this.secondDigit]
+    }
+
+    @LazyGetter()
+    get isLengthValid() {
+        return this.digits.length === 11;
+    }
+
+    @LazyGetter()
+    get isEveryCharTheSame(): boolean {
+        return this.digits.every(c => c === this.digits[0]);
+    }
+
+    @LazyGetter()
+    get areVerifierDigitsValid(): boolean {
+        return this.givenVerifierDigits.toString() === this.calculatedVerifierDigits.toString()
+    }
+
+    @LazyGetter()
+    get isValid() {
+        return this.isLengthValid && !this.isEveryCharTheSame && this.areVerifierDigitsValid
+    }
 }
