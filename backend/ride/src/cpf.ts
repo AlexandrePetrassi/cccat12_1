6e7990ac9624f5ae6a7b013cf603a6a0f46fe9e6
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-03 22:13:20 -0300

    refactor: extrair cpf#reduceByFactorOf

--- backend/ride/src/cpf.ts
+++ backend/ride/src/cpf.ts
@@ -18,14 +18,18 @@ function isLengthValid(cpf: string) {
     return cpf.length === 11;
 }
 
+function reduceByFactorOf(factor: number) {
+    return (prev: number, next: number, index: number) => prev + (factor - index) * next
+}
+
 function calculateFirstDigit(digits: number[]) {
-    let d1 = digits.reduce((prev, next, nCount) => prev + (10 - nCount) * next, 0)
+    let d1 = digits.reduce(reduceByFactorOf(10), 0)
     const rest = (d1 % 11);
     return (rest < 2) ? 0 : 11 - rest;
 }
 
 function calculateSecondDigit(digits: number[], d1: number) {
-    let d2 = digits.reduce((prev, next, nCount) => prev + (11 - nCount) * next, 0)
+    let d2 = digits.reduce(reduceByFactorOf(11), 0)
     const rest2 = ((d2 + 2 * d1) % 11);
     return rest2 < 2 ? 0 : 11 - rest2;
 }
