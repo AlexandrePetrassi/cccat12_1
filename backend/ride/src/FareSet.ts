30ca2f8560ab884ea101f590082fd7e3437776f3
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-04 22:54:39 -0300

    Refactor: FareSet e Ride utilizam Fare

--- backend/ride/src/FareSet.ts
+++ backend/ride/src/FareSet.ts
@@ -1,17 +1,18 @@
 import Segment from "./Segment";
+import {Fare} from "./Fare";
 
 export class FareSet {
     constructor(
-        readonly strategies: [condition: (segment: Segment) => boolean, value: number][] = [],
+        readonly strategies: Fare[] = [],
         readonly defaultValue = 1
     ) {}
 }
 
 export const DEFAULT_FARE = new FareSet(
     [
-        [it => it.isOvernight() && it.isSunday(), 5],
-        [it => it.isOvernight() && !it.isSunday(), 3.90],
-        [it => !it.isOvernight() && it.isSunday(), 2.9],
+        new Fare(it => it.isOvernight() && it.isSunday(), 5),
+        new Fare(it => it.isOvernight() && !it.isSunday(), 3.90),
+        new Fare(it => !it.isOvernight() && it.isSunday(), 2.9),
     ],
     2.1
 )
--- backend/ride/src/Ride.ts
+++ backend/ride/src/Ride.ts
@@ -1,10 +1,24 @@
 import Segment from "./Segment";
 import {DEFAULT_FARE, FareSet} from "./FareSet";
 
+function calculateRideTotalPrice(segments: Segment[], fareSet: FareSet, minimumPrice: number) {
+	let price = 0
+	for (const segment of segments) {
+		let value =  fareSet.defaultValue
+		for (const fare of fareSet.strategies) {
+			if (fare.condition(segment)) {
+				value = fare.value
+			}
+		}
+		price += value * segment.distance
+	}
+	return (price < minimumPrice) ? minimumPrice : price;
+}
+
 export default class Ride {
 	constructor (
 		readonly segments: Segment[] = [],
-		readonly fare: FareSet = DEFAULT_FARE,
+		readonly fareSet: FareSet = DEFAULT_FARE,
 		readonly minimumPrice: number = 10
 	) {}
 
@@ -12,21 +26,7 @@ export default class Ride {
 		this.segments.push(new Segment(distance, date));
 	}
 
-	ofSegment(segment: Segment): number {
-		for (const [condition, value] of this.fare.strategies) {
-			if (condition(segment)) return value * segment.distance
-		}
-		return this.fare.defaultValue * segment.distance
-	}
-
-	plus(a: number, b: number) {
-		return a + b
-	}
-
-	calculate () {
-		const price = this.segments
-			.map(this.ofSegment)
-			.reduce(this.plus, 0)
-		return (price < this.minimumPrice) ? this.minimumPrice : price;
+	calculate (): number {
+		return calculateRideTotalPrice(this.segments, this.fareSet, this.minimumPrice)
 	}
 }
