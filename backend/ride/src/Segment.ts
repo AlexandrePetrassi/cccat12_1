34738f42771252011856ba67226718af09b01383
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-05 20:49:38 -0300

    Add: Segment#value

--- backend/ride/src/Segment.ts
+++ backend/ride/src/Segment.ts
@@ -1,8 +1,11 @@
+import {DEFAULT_FARE_SET, FareSet} from "./FareSet";
+import {LazyGetter} from "lazy-get-decorator";
+
 export default class Segment {
 
-	constructor (readonly distance: number, readonly date: Date) {
-		if (!this.isValidDistance()) throw new Error("Invalid distance");
-		if (!this.isValidDate()) throw new Error("Invalid date");
+	constructor (readonly distance: number, readonly date: Date, readonly fareSet: FareSet = DEFAULT_FARE_SET) {
+		if (!isValidDistance(distance)) throw new Error("Invalid distance");
+		if (!isValidDate(date)) throw new Error("Invalid date");
 	}
 
 	isOvernight () {
@@ -12,12 +15,21 @@ export default class Segment {
 	isSunday () {
 		return this.date.getDay() === 0;
 	}
-	
-	isValidDistance () {
-		return this.distance && typeof this.distance === "number" && this.distance > 0;
+
+	@LazyGetter()
+	get value(): number {
+		return segmentValue(this.fareSet, this)
 	}
-	
-	isValidDate () {
-		return this.date && this.date instanceof Date && this.date.toString() !== "Invalid Date";
-	}
+}
+
+function segmentValue(fareSet: FareSet, segment: Segment) {
+	return fareSet.strategies.find(fare => fare.condition(segment))?.value ?? fareSet.defaultValue;
+}
+
+function isValidDistance (distance: number) {
+	return distance > 0;
+}
+
+function isValidDate (date: Date) {
+	return date.toString() !== "Invalid Date";
 }
