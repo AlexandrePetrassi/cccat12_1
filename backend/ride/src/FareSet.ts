88e8b3ed72103b7bd8941b4f3575413d87808190
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-07-05 21:13:57 -0300

    Add: FareSet#fairOf

--- backend/ride/src/FareSet.ts
+++ backend/ride/src/FareSet.ts
@@ -1,10 +1,15 @@
 import {Fare} from "./Fare";
+import Segment from "./Segment";
 
 export class FareSet {
     constructor(
         readonly strategies: Fare[] = [],
         readonly defaultValue = 1
     ) {}
+
+    fairOf(segment: Segment): number {
+        return this.strategies.find(fare => fare.condition(segment))?.value ?? this.defaultValue;
+    }
 }
 
 export const DEFAULT_FARE_SET = new FareSet(
